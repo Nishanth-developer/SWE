b9cfa1b9d5821f07b98436c323485da8d2e26ec9
Nishanth-developer <augustcoder24x7@gmail.com>
2022-11-15 01:00:54 +0530

    Added image compression

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "storage")));
-app.use(fileUpload());
+// app.use(fileUpload());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -43,84 +43,97 @@ app.get("/", function (request, res) {
   res.render("index");
 });
 app.post("/", function (request, res) {
-	console.log("hi");
+	console.log(request);
+  //   var mail_check = regex_checker_mail(email);
+  //   var reg_check = regex_checker_reg(reg_no);
+  //   if(mail_check===1 && reg_check===1){
+  //      if (!req.files)
+  // 			return res.status(400).send('No files were uploaded.');
+
   const image = request.files.image;
   if (image.size > 0) {
-    if (image.type == "image/png" || image.type == "image/jpeg") {
-      fileSystem.readFile(image.path, function (error, data) {
+    fileSystem.readFile(image.path, function (error, data) {
+      if (error) throw error;
+
+      const filePath = "uploads/" + image.name;
+      const compressedFilePath = "uploadscomp/" + image.name;
+      console.log(filePath, compressedFilePath);
+      const compression = 80;
+      fileSystem.writeFile(filePath, data, async function (error) {
         if (error) throw error;
 
-        const filePath =
-          "temp-uploads/" + new Date().getTime() + "-" + image.name;
-        const compressedFilePath = "uploads/";
-        const compression = 60;
-		console.log("hello");
-        fileSystem.writeFile(filePath, data, async function (error) {
-          if (error) throw error;
-
-          compressImages(
-            filePath,
-            compressedFilePath,
-            { compress_force: false, statistic: true, autoupdate: true },
-            false,
-            { jpg: { engine: "mozjpeg", command: ["-quality", compression] } },
-            {
-              png: {
-                engine: "pngquant",
-                command: ["--quality=" + compression + "-" + compression, "-o"],
-              },
+        compressImages(
+          filePath,
+          compressedFilePath,
+          { compress_force: false, statistic: true, autoupdate: true },
+          false,
+          { jpg: { engine: "mozjpeg", command: ["-quality", compression] } },
+          {
+            png: {
+              engine: "pngquant",
+              command: ["--quality=" + compression + "-" + compression, "-o"],
             },
-            { svg: { engine: "svgo", command: "--multipass" } },
-            {
-              gif: {
-                engine: "gifsicle",
-                command: ["--colors", "64", "--use-col=web"],
-              },
+          },
+          { svg: { engine: "svgo", command: "--multipass" } },
+          {
+            gif: {
+              engine: "gifsicle",
+              command: ["--colors", "64", "--use-col=web"],
             },
-            async function (error, completed, statistic) {
-              console.log("-------------");
-              console.log(error);
-              console.log(completed);
-              console.log(statistic);
-              console.log("-------------");
-
-              fileSystem.unlink(filePath, function (error) {
-                if (error) throw error;
-              });
-            }
-          );
-		  console.log("hello2");
-          var sql =
-            "INSERT INTO `products`(`name`,`mobile`,`reg_no`,`mail`, `image` ,`price`,`product_name`,`category`,`time_used`) VALUES ('" +
-            name +
-            "','" +
-            mobile +
-            "','" +
-            reg_no +
-            "','" +
-            email +
-            "','" +
-            img_name +
-            "','" +
-            price +
-            "','" +
-            product_name +
-            "','" +
-            category +
-            "','" +
-            time_used +
-            "')";
-
-          db.query(sql, function (err, result) {
-            res.redirect("/buy");
-          });
-        });
-
-        fileSystem.unlink(image.path, function (error) {
-          if (error) throw error;
-        });
+          },
+          async function (error, completed, statistic) {
+            message = "";
+            var post = request.fields;
+            var name = post.name;
+            var mobile = post.mobile;
+            var reg_no = post.reg_no;
+            var email = post.email;
+            var price = post.price;
+            var category = post.category;
+            var product_name = post.product_name;
+            var time_used = post.time_used;
+            console.log(name, mobile);
+            var img_name = request.files.image.name;
+            console.log("-------------");
+            console.log(error);
+            console.log(completed);
+            console.log(statistic);
+            console.log("-------------");
+            var sql =
+              "INSERT INTO `products`(`name`,`mobile`,`reg_no`,`mail`, `image` ,`price`,`product_name`,`category`,`time_used`) VALUES ('" +
+              name +
+              "','" +
+              mobile +
+              "','" +
+              reg_no +
+              "','" +
+              email +
+              "','" +
+              img_name +
+              "','" +
+              price +
+              "','" +
+              product_name +
+              "','" +
+              category +
+              "','" +
+              time_used +
+              "')";
+           db.query(sql, function (err, result) {
+              res.redirect("/buy");
+              console.log(err);
+            });
+            fileSystem.unlink(filePath, function (error) {
+              if (error) throw error;
+            });
+          }
+        );
       });
-    }
+
+      fileSystem.unlink(image.path, function (error) {
+        if (error) throw error;
+      });
+    });
   }
 });
 
